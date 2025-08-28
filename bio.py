c0d0cfc1ebbd3df13b7cc07255e4dcb1db219ff1
Daniel Johnson <38923374+mcchavburger@users.noreply.github.com>
2025-08-28 15:32:37 +0100

    Delete bio.py

--- bio.py
+++
@@ -1,9 +0,0 @@
-name = input("What is your name? ")
-colour = input("What is your favourtite Color? ")
-age = int(input ("How old are you today? "))
-
-#print(name, end=' ')
-#print("is " + str(age) + " years old", end=' ')
-#print("and loves the colour " + colour + ".", end=)
-
-print(name, 'is', age, 'years old and loves the colour', colour, '.', sep=" ")
